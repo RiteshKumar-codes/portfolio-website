14ec216bf72f6161ca2208aa9374403f6cd59070
RiteshKumar-codes <riteshk7710@gmail.com>
2026-06-14 10:59:47 +0530

    Initial portfolio setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import "../styles/Hero.css";
+
+function Hero(){
+    return(
+        <section className="hero">
+            <h1>Hi, I'm Ritesh Kumar</h1>
+            <h2>Software Developer</h2>
+            <p>
+                Last year CSE Student passionate about
+                Web Devlopment and Web Technologies.  
+            </p>
+
+            <button>Download Resume</button>
+        </section>
+    );
+}
+
+export default Hero;
